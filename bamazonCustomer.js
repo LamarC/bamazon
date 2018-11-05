0daafe9fd7669487c8b79e2440662a2f0bdf05af
LAPTOP\Diana <dviann89@gmail.com>
2018-11-05 00:15:42 -0600

    Testing update of products

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,20 +4,76 @@ const inquirer = require('inquirer');
 const table = require('cli-table');
 const chalk = require('chalk');
 
-
-
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port:3306,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  host:     'localhost',
+  port:     3306,
+  username: 'user',
+  password: 'password',
   database: 'bamazon'
 });
 
-
+//Establish connetion 
 connection.connect(function(err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  connection.end();
+  console.log(chalk.blue('Welcome to Bamazon database as id ' + connection.threadId));
+  connection.end();  
+
+  selectProduct();
 });
 
+function selectProduct() {
+  connection.query('SELECT * FROM products', function(err, res){
+    if (err) throw err;
+
+    //Display table with selected colors
+    let table = new Table(
+      {
+        head:['Product ID'.cyan.bold, 'Product Name', cyan.bold, 'Department Name'.cyan.bold, 'Price'.cyan.bold, 'Quantity'.cyan.bold],
+        colWidths: [10, 70, 70, 15, 10],
+      });
+
+    //
+    for(let i=0; i < res.length; i++) {
+      table.push(
+        [res(i).id, res(i).product_name, res(i).department_name, parseFloat(res(i).price).toFixed, res(i).stock_quantity]
+      );
+    }
+
+    console.log(table.toString());
+
+    inquirer.prompt([
+      {
+        type: 'number',
+        message: 'Please enter the Item ID of the product that you would like to purchase.'.green,
+        name: 'id'
+      },
+      {
+        type: 'number',
+        message: 'How many would you like?'.green,
+        name: 'quantity'
+      },
+    ])
+
+    .then(function (cart) {
+      let quantity = cart.quantity;
+      let itemID = cart.id;
+
+      connection.query('SELECT * FROM products WHERE id= ' + itemID, function(err, selectedItem){
+        if (err) throw err;
+
+        if (quantity <= selectedItem.stock_quantity) {
+          console.log('The product selected is in stock'.magenta, 'Order is being placed'.bgMagenta);
+
+          let updateQuantity = 'UPDATE products SET stock_quantity = ' + selectedItem.stock_quantity;
+
+          connection.query('UPDATE products SET stock_quantity=? WHERE id=?', selectedItem.stock_quantity)
+        }
+      })  
+
+    })
+
+
+
+
+  })
+}
--- bamazon_schema.sql
+++ bamazon_schema.sql
@@ -12,3 +12,15 @@ CREATE table products (
     stock_quantity INT(25) DEFAULT NULL,
     PRIMARY KEY (id)
 );
+
+INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('Echo Dot', 'Electronics', 29.99, 30);
+INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('Cards Against Humanity','Toys / Board Games', 25.00, 17);
+INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('Nintendo - Switch 32GB Console', 'Electronics', 299.99, 5);
+INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('Women Hooded Knit Cardigans' 'Apparel', 41.99, 10);
+INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('Ripped Boyfriend Jeans', 'Apparel', 26.75, 8);
+INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('Ceramic Hanging Planter ' 'Home Decor', 22.50, 45);
+INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('15-Piece Non-Stick Cookware Set', 'Kitchen & Dining', 159.99, 3);
+INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('Vans U Authentic, Unisex', 'Apparel', 33.95, 84);
+INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('School Backpack Vintage', 'Luggage & Travel Gear', 36.99, 1);
+INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('Paula: A Memoir', 'Books & Audibles', 12.95, 26);
+
